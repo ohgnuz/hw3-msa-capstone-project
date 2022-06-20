b7fc5e84c5e3ed61c2f99c047d62901c461423aa
89sooner <slrttt2001@naver.com>
2022-06-20 06:25:55 +0000

    capstone project remove

--- capstone-domain/Delivery/src/test/java/hwmsacapstoneteam/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package hwmsacapstoneteam.common;
-
-import hwmsacapstoneteam.DeliveryApplication;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { DeliveryApplication.class })
-public class CucumberSpingConfiguration {}
--- capstone-domain/Pay/src/test/java/hwmsacapstoneteam/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package hwmsacapstoneteam.common;
-
-import hwmsacapstoneteam.PayApplication;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { PayApplication.class })
-public class CucumberSpingConfiguration {}
