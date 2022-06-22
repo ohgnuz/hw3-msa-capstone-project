765834a98c564b73c3218e941be3099f43e2415a
89sooner <slrttt2001@naver.com>
2022-06-22 00:07:47 +0000

    capston-domain drop

--- capstone-domain/Order/src/test/java/hwmsacapstoneteam/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package hwmsacapstoneteam.common;
-
-import hwmsacapstoneteam.OrderApplication;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { OrderApplication.class })
-public class CucumberSpingConfiguration {}
--- capstone-domain/Product/src/test/java/hwmsacapstoneteam/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package hwmsacapstoneteam.common;
-
-import hwmsacapstoneteam.ProductApplication;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { ProductApplication.class })
-public class CucumberSpingConfiguration {}
